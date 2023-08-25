04e82d718b592bd89a04df4ba5544cbee2f1c8f4
fujifx <fuzuli@gmail.com>
2023-08-25 10:59:09 +1000

    Delete path Changing response Status Codes

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from random import randrange
 from typing import Optional
-from fastapi import Body, FastAPI
+from fastapi import Body, FastAPI, HTTPException, Response, status
 from pydantic import UUID4, BaseModel
 
 app = FastAPI()
@@ -24,6 +24,11 @@ def find_post(id):
     for post in my_posts:
         if post['id'] == id:
             return post
+        
+def find_index_post(id):
+    for i, p in enumerate(my_posts):
+        if p['id'] == id:
+            return i
 
 @app.get("/")
 async def root():
@@ -33,7 +38,7 @@ async def root():
 async def get_posts():
     return {"data": my_posts}
 
-@app.post("/createposts")
+@app.post("/createposts", status_code=status.HTTP_201_CREATED)
 async def create_posts(post: Post):
     post_dict = post.model_dump()   # converts Pydantic model into Python Dictionary. .dict is deprecated
     post_dict['id'] = randrange(0, 1000000)
@@ -47,7 +52,28 @@ async def get_latest_post():
     return {"data": post}
 
 @app.get("/posts/{id}")
-async def get_post(id: int):
+async def get_post(id: int, response: Response):
     # in the above specifying the parameter type int would enable FastAPI to enforce parameter type as int
     post = find_post(id)
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} was not found")
     return{"data": post}
+
+@app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_post(id: int):
+    # Deleting a post
+    # find the index in the array that has required ID
+    post_index = find_index_post(id)
+
+    if post_index == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} was not found")
+    
+    my_posts.pop(post_index)
+
+    # When the status code is 204, you are not supposed send back any data, 
+    # if done so you'd end up with the error 'Too much data for declated Content-Length'
+    # return{'message': f"Post with id {id} successfully deleted"}
+
+    # So instead, return as below
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+ 
